6aa69f723b7124c4c09eead8b6cadc259530dba0
yoonjisu1201 <yoonjisu1201@gmail.com>
2026-04-29 16:00:18 +0900

    테스트

---
+++ Graph.cs
@@ -0,0 +1,57 @@
+public class Graph
+{
+    public int rows = 0;
+    public int cols = 0;
+
+    public GraphNode[] nodes;
+
+    public void Init(int[,] grid)
+    {
+        rows = grid.GetLength(0);
+        cols = grid.GetLength(1);
+
+        nodes = new GraphNode[grid.Length];
+        for (int i = 0; i < nodes.Length; ++i)
+        {
+            nodes[i] = new GraphNode();
+            nodes[i].id = i;
+        }
+
+        for (int r = 0; r < rows; ++r)
+        {
+            for (int c = 0; c < cols; ++c)
+            {
+                int index = r * cols + c;
+                nodes[index].weight = grid[r, c];
+
+                if (grid[r, c] == -1)
+                    continue;
+
+                if (r -1 >= 0 && grid[r - 1, c] >= 0)
+                {
+                    nodes[index].adjacents.Add(nodes[index - cols]); // u
+                }
+                if (c + 1 < cols && grid[r, c + 1] >= 0)
+                {
+                    nodes[index].adjacents.Add(nodes[index + 1]); // r
+                }
+                if (r + 1 < rows && grid[r + 1, c] >= 0)
+                {
+                    nodes[index].adjacents.Add(nodes[index + cols]); // d
+                }
+                if (c - 1 >= 0 && grid[r, c - 1] >= 0)
+                {
+                    nodes[index].adjacents.Add(nodes[index - 1]); // l
+                }
+            }
+        }
+    }
+
+    public void ResetNodePrevious()
+    {
+        foreach (var node in nodes)
+        {
+            node.previous = null;
+        }
+    }
+}
